3efd5e977f388ecb1db7c8a5239c640fcc022f8d
ahmed <ae674499@gmail.com>
2025-11-15 18:01:42 +0200

    added course class

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
-}
+    private final String courseId, title , description , instructorId ;
+    private List<Lesson> lessons = new ArrayList<>();
+    private List<String> students = new ArrayList<>();
+
+    public Course(String courseId, String title, String description, String instructorId) {
+        this.courseId = courseId;
+        this.title = title;
+        this.description = description;
+        this.instructorId = instructorId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<String> students) {
+        this.students = students;
+    }
+    public void enrollStudent(String studentId) {
+        if (!students.contains(studentId)) {
+            students.add(studentId);
+        }
+    }
+    public void addLesson(Lesson lesson) {
+        lessons.add(lesson);
+    }
+    public void deleteLesson(String lessonId) {
+        for (int i = 0; i < lessons.size(); i++) {
+            if (lessons.get(i).getLessonId().equals(lessonId)) {
+                lessons.remove(i);
+                return;  }
+            } }
+
+    }
