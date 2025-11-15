555b8fc76f3a095a1490c19c0c17b44f23ddbc08
Yassin <YassinKhaled911@gmail.com>
2025-11-16 01:47:38 +0200

    login added

---
+++ src/LoginFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame {
+    private JPanel panel1;
+    private JPasswordField passWord;
+    private JTextField textField1;
+    private JTextField userNameTextField;
+    private JTextField passwordTextField;
+
+    public LoginFrame() {
+        s
+        passWord.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        textField1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
