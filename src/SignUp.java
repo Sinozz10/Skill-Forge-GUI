ef72fadc6198ff166858f656dca039e4e27afae1
Yassin <YassinKhaled911@gmail.com>
2025-11-16 18:47:42 +0200

    finished User SignUp

---
+++ src/SignUp.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignUp extends JFrame {
+    private JPanel SignUp;
+    private JPasswordField ConfirmedPass;
+    private JTextField userName;
+    private JTextField Email;
+    private JPasswordField Password;
+    private JComboBox<String> role;
+    private JButton backButton;
+    private JButton signUpButton;
+
+    private JsonDatabaseManager database;
+    private AuthenticateManager authManager;
+
+    public SignUp() {
+        setTitle("SignUp");
+        setContentPane(SignUp);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+
+        userName.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Email.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        role.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ConfirmedPass.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Password.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new entryFrame();
+
+            }
+        });
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleSignUp();
+            }
+        });
+    }
+
+    private void handleSignUp() {
+        String username = userName.getText();
+        String email = Email.getText();
+        String password = new String(Password.getPassword());
+        String confirmedPass = new String(ConfirmedPass.getPassword());
+        String Role = (String) role.getSelectedItem();
+
+        if(!password.equals(confirmedPass)){
+            JOptionPane.showMessageDialog(null, "Passwords do not match", "Error", JOptionPane.ERROR_MESSAGE);
+            ConfirmedPass.setText("");
+            Password.requestFocus();
+        }
+
+        try {
+
+            User newUser = authManager.signup(username, email, password, Role);
+            System.out.println("Status: SUCCESS");
+            System.out.println("User ID: " + newUser.getuID());
+
+            if (role.equals("Student")) {
+                System.out.println(" NEW STUDENT REGISTERED");
+                System.out.println(" Name: " + newUser.getUsername());
+                System.out.println(" Email: " + newUser.getEmail());
+                System.out.println(" ID: " + newUser.getuID());
+            } else if (role.equals("Instructor")) {
+                System.out.println(" NEW INSTRUCTOR REGISTERED");
+                System.out.println(" Name: " + newUser);
+                System.out.println(" Email: " + newUser);
+                System.out.println(" ID: " + newUser);
+            }
+
+            JOptionPane.showMessageDialog(this, "Account created successfully!\nWelcome, " + newUser.getUsername() + "You can now sign in with your credentials.", "Success", JOptionPane.INFORMATION_MESSAGE);
+            LoginFrame loginFrame = new LoginFrame();
+        } catch (IllegalArgumentException e){
+            System.out.println("Status: FAILED - " + e.getMessage());
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Signup Failed", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
