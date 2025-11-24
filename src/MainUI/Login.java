c8fc098922720e6e40a9a39ed6c96839fb572631
Yassin <YassinKhaled911@gmail.com>
2025-11-24 03:02:16 +0200

    edited login

--- src/MainUI/Login.java
+++ src/MainUI/Login.java
@@ -27,79 +27,77 @@ public class Login extends JPanel {
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.anchor = GridBagConstraints.CENTER;
 
-        // Title
-        JLabel titleLabel = new JLabel("SKILL FORGE SYSTEM");
-        titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 38));
+        JLabel titleSystem = new JLabel("SKILL FORGE SYSTEM");
+        titleSystem.setForeground(Color.WHITE);
+        titleSystem.setFont(new Font("Arial", Font.BOLD, 38));
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
         gbc.insets = new Insets(20, 5, 30, 5);
-        add(titleLabel, gbc);
+        add(titleSystem, gbc);
 
-        // Username Label
         gbc.gridwidth = 1;
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.gridy = 1;
         gbc.gridx = 0;
         gbc.anchor = GridBagConstraints.EAST;
-        JLabel userLabel = new JLabel("Username: ");
-        userLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-        add(userLabel, gbc);
+        JLabel userNAME = new JLabel("Username: ");
+        userNAME.setFont(new Font("Arial", Font.PLAIN, 14));
+        add(userNAME, gbc);
 
-        // Username Field
         gbc.gridx = 1;
         gbc.anchor = GridBagConstraints.CENTER;
         userName = new JTextField(20);
         userName.setPreferredSize(new Dimension(200, 25));
         add(userName, gbc);
 
-        // Password Label
+
         gbc.gridy = 2;
         gbc.gridx = 0;
         gbc.anchor = GridBagConstraints.EAST;
-        JLabel passLabel = new JLabel("Password: ");
-        passLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-        add(passLabel, gbc);
+        JLabel passWORD = new JLabel("Password: ");
+        passWORD.setFont(new Font("Arial", Font.PLAIN, 14));
+        add(passWORD, gbc);
 
-        // Password Field
+
         gbc.gridx = 1;
         gbc.anchor = GridBagConstraints.CENTER;
         passWord = new JPasswordField(20);
         passWord.setPreferredSize(new Dimension(200, 25));
         add(passWord, gbc);
 
-        // Login Button
+
         gbc.gridy = 3;
         gbc.gridx = 0;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.insets = new Insets(20, 5, 5, 5);
-        JButton loginButton = new JButton("Login");
-        loginButton.setPreferredSize(new Dimension(120, 35));
-        loginButton.setForeground(Color.black);
-        loginButton.setBackground(Color.LIGHT_GRAY);
-        loginButton.setFont(new Font("Arial", Font.BOLD, 14));
-        loginButton.setMargin(new Insets(3, 10, 3, 10));
-        add(loginButton, gbc);
+        JButton loginBtn = new JButton("Login");
+        loginBtn.setPreferredSize(new Dimension(120, 35));
+        loginBtn.setForeground(Color.black);
+        loginBtn.setBackground(Color.LIGHT_GRAY);
+        loginBtn.setFont(new Font("Arial", Font.BOLD, 14));
+        loginBtn.setMargin(new Insets(3, 10, 3, 10));
+        add(loginBtn, gbc);
 
-        // Panel
+        // mawdoo el not a member
         gbc.gridy = 4;
         gbc.insets = new Insets(30, 5, 5, 5);
         JPanel signupPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 0));
         JLabel notMemberLabel = new JLabel("Not a Member?");
-        notMemberLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-        JButton signupButton = new JButton("Signup");
-        signupButton.setPreferredSize(new Dimension(100, 30));
-        signupButton.setForeground(Color.black);
-        signupButton.setBackground(Color.LIGHT_GRAY);
-        signupButton.setFont(new Font("Arial", Font.BOLD, 14));
-        signupButton.setMargin(new Insets(3, 10, 3, 10));
+        notMemberLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        JButton signupBtn = new JButton("Signup");
+        signupBtn.setPreferredSize(new Dimension(100, 30));
+        signupBtn.setForeground(Color.black);
+        signupBtn.setBackground(Color.LIGHT_GRAY);
+        signupBtn.setFont(new Font("Arial", Font.BOLD, 14));
+        signupBtn.setMargin(new Insets(3, 10, 3, 10));
         signupPanel.add(notMemberLabel);
-        signupPanel.add(signupButton);
+        signupPanel.add(signupBtn);
         add(signupPanel, gbc);
 
-        // Event Listeners
+
+       //kol el listeners b-listeno lel enter.
         userName.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -118,9 +116,8 @@ public class Login extends JPanel {
             }
         });
 
-        loginButton.addActionListener(_ -> handleLogin());
-
-        signupButton.addActionListener(_ -> ld.changeContentPanel(new SignUp(ld)));
+        loginBtn.addActionListener(_ -> handleLogin());
+        signupBtn.addActionListener(_ -> ld.changeContentPanel(new SignUp(ld)));
     }
 
     private void handleLogin() {
